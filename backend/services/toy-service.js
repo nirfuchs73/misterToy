80a9c42771e86dfe80ee9e802e0d07eae3c4e934
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-13 17:07:36 +0200

    Update

--- backend/services/toy-service.js
+++ backend/services/toy-service.js
@@ -2,15 +2,19 @@ const mongoService = require('./mongo-service')
 
 const ObjectId = require('mongodb').ObjectId;
 
-
-// function checkLogin({ nickname }) {
-//     return mongoService.connect()
-//         .then(db => db.collection('user').findOne({ nickname }))
-// }
-
 function query() {
     return mongoService.connect()
         .then(db => db.collection('toys').find({}).toArray())
+}
+
+function remove(toyId) {
+    const id = new ObjectId(toyId)
+    return mongoService.connect()
+        .then(db => db.collection('toys').remove({ _id: id }))
+        .then(res => {
+            // toy._id = res.insertedId
+            // return toy;
+        })
 }
 
 function getById(id) {
@@ -19,7 +23,6 @@ function getById(id) {
         .then(db => db.collection('toys').findOne({ _id }))
 }
 
-// todo  - add user only if nickname is not taken
 function add(toy) {
     return mongoService.connect()
         .then(db => db.collection('toys').insertOne(toy))
@@ -34,26 +37,14 @@ function update(toy) {
     return mongoService.connect()
         .then(db => db.collection('toys').updateOne({ _id: toy._id }, { $set: toy }))
         .then(res => {
-            // toy._id = res.insertedId
             return toy;
-        })
-}
-
-// db.customer.remove({"_id":ObjectId("579c6ecab87b4b49be12664c")})
-function remove(toyId) {
-    const id = new ObjectId(toyId)
-    return mongoService.connect()
-        .then(db => db.collection('toys').remove({ _id: id }))
-        .then(res => {
-            // toy._id = res.insertedId
-            // return toy;
         })
 }
 
 module.exports = {
     query,
+    remove,
     getById,
     add,
-    update,
-    remove
+    update
 }
